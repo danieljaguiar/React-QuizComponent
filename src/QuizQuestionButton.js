dfc06f8fd681b474139130b48c45c16ad8bc8601
Daniel Aguiar <daguia@microsoft.com>
2020-04-23 15:56:51 -0500

    4.1 Done

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class QuizQuestionButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <li>
+        <button></button>
+      </li>
+    );
+  }
+}
+
+export default QuizQuestionButton;
